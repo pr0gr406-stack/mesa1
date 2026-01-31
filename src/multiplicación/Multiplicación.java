5f22deeee19a6841e652ed16d6d13aeecf8ecafa
Pc2 <Pc2@Villapc2>
2026-01-31 09:42:14 -0600

    CodigoPrincipal

---
+++ src/multiplicación/Multiplicación.java
@@ -0,0 +1,21 @@
+
+package multiplicación;
+
+import java.util.Scanner;
+
+public class Multiplicación {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingresa opcion: ");
+        int a = sc.nextInt();
+        System.out.println("Ingresa el segundo numero: ");
+        int b = sc.nextInt();
+        
+       // Multiplicacion multiplicacion = new multiplicacion(); 
+        
+        //System.out.println("La multiplicacion es "+multiplicacion.calculadora(a,b));
+    }
+    
+}
